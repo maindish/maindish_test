63b7aa85f41372a710dafa24860e516a7bd63c40
maindish <you46@naver.com>
2015-01-13 18:11:05 +0900

    Xcode test C source file
    Xcode 울을 이용한 Mac에서의 C 프로그래밍을 위한 처음 시작한 테스트 파일입니다.

--- SockProgramming/main.c
+++ SockProgramming/main.c
@@ -7,9 +7,46 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(int argc, const char * argv[]) {
+/*int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    return 0;
+}*/
+
+int main(int argc, char * argv[])
+{
+    int firstAge, secondAge;
+    char operation;
+    bool finished;
+    
+    finished = false;
+    
+    while (finished != true) {
+        printf("What operation do you want to do?\n");
+        
+        scanf("%c", &operation);
+        fpurge(stdin);
+        
+        if (operation == '+') {
+            printf("Enter left argument: ");
+            scanf("%d", &firstAge);
+            fpurge(stdin);
+            
+            printf("Enter right argument: ");
+            scanf("%d", &secondAge);
+            fpurge(stdin);
+            
+            printf("\n%d + %d = %d\n", firstAge, secondAge, (firstAge + secondAge));
+        }
+        else
+        {
+            finished = true;
+        }
+    }
+    
+    printf("Finished.\n");
+    
     return 0;
 }
