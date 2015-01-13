6de6ef1b5968a2a7b0925190e3b7ae27d0b7a70d
Maindish <Maindish@Maindishui-MacBook-Air.local>
2015-01-13 13:32:35 +0900

    Initial Commit

---
+++ SockProgramming/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SockProgramming
+//
+//  Created by Maindish on 2015. 1. 13..
+//  Copyright (c) 2015년 Maindish. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
